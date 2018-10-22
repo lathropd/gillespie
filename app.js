92bf1aee094569d9aa8b086ba419b05c93c03c02
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-10-22 14:40:21 -0500

    add grunt

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+
 
 // response
 
+
+app.use(router.routes())
+  .use(router.allowedMethods());
+
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
+  if (ctx.body == null) {
+    ctx.body = 'Hello Koa';
+  }
 });
 
 app.listen(1337);
